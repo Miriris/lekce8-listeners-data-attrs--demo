13712140b04cca9096ce2ce14cd828a5e776837a
Miriam <rezakmiriam@gmail.com>
2022-04-05 19:09:49 +0200

    add event listener

--- ovoce.js
+++ ovoce.js
@@ -1,8 +1,8 @@
 //console.log('test');
 let prvniOvoce = document.querySelector('#muj-div').dataset.ovoce;
 let druheOvoce = document.querySelector('#muj-druhy-div').dataset.ovoce;
-console.log(prvniOvoce);
-console.log(druheOvoce);
+//console.log(prvniOvoce);
+//console.log(druheOvoce);
 
 let dataUrl = document.querySelector('body').dataset.dataUrl;
 console.log(dataUrl);
